8e9977d296eab7a208f6892a867e4c45346b585c
Yicheng Jin <bobjin42@gmail.com>
2018-06-21 19:19:31 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,43 @@
 # your code goes here
+def begins_with_r(arr)
+  arr.each{|ele| 
+    if ele[0] != "r"
+      return false
+    end 
+  }
+  return true
+end  
+
+def contain_a(arr)
+  new_arr = []
+  arr.each{|ele|
+    ele.split("").each{|char| 
+      if char == "a"
+      new_arr << ele
+    end
+    }
+  }
+  return new_arr
+end
+
+def first_wa(arr)
+  arr.each{|ele|
+    if ele[0..1] == "wa"
+      return ele
+    end
+  }
+end
+
+def remove_non_strings(arr)
+  new_arr = []
+  arr.each{|ele|
+    if ele.to_s == ele
+     new_arr << ele
+    end
+  }
+  return new_arr
+end
+
+def count_elements(arr)
+
+end
