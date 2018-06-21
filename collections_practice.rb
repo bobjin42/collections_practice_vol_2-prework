d6335a84f034ec7f3afb4dcc389b476884b9fe16
Yicheng Jin <bobjin42@gmail.com>
2018-06-21 20:22:29 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -71,7 +71,7 @@ def organize_schools(schools)
   schools.each{|school,data|
     data.each{|location, value|
       if new_hash[value] == nil
-        new_hash[value] == [school]
+        new_hash[value] = [school]
       else
         
     }
